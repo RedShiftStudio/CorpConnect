5af10eb015be004c6d3738b87f0f7873b3e87e75
KuiYL <hyungwonaaaaaa@gmail.com>
2025-04-27 15:01:42 +0300

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.company.corpconnect"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.company.corpconnect"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Firebase (Push-уведомления)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.messaging)
+
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    // Сеть (Retrofit + GSON)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+    // Локальная база данных (Room)
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+
+    // Асинхронность (Coroutines)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Загрузка изображений (Glide)
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+}
